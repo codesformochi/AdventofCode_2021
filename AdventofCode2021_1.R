6d771ea88bfe5d6bf50b7dc97794d34fc3d58790
zhanl238 <zhanl238@gene.com>
2022-04-11 16:57:04 -0700

    cleaning up D1P1

--- AdventofCode2021_1.R
+++ AdventofCode2021_1.R
@@ -1,39 +1,17 @@
 #libraries
+library(rtricks)
 
 #PART 1
 #example
-directions <- c(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
-
-all_elements <- 1:length(directions)
-
-#function to determine whether increasing or decreasing
-fu <- function(x, position){
-  dataset <- x
-  print(position)
-
-  #if not the first element...
-  if(position != 1){
-
-    #compare whether the element is > the last
-    print(dataset[position] > dataset[position-1])
-
-  #else then NA
-  }else{
-    "NA"
-  }
-} #fu end
-
-#for all elements, apply the function.
-#get number of elements that are > the last element
-results <- lapply(all_elements, FUN = fu, x = directions)
-sum(results == "TRUE")
+#' directions <- c(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+#' all_elements <- 1:length(directions)
+#' results <- lapply(all_elements, FUN = compare_x_to_xminusone, x = directions)
 
 #Read in csv file
 directions <- read.csv("AOC2021_1.txt", header = FALSE)
-as.list(directions)
 
 all_elements <- 1:nrow(directions)
-results <- mapply(all_elements, FUN = fu, x = directions)
+results <- mapply(all_elements, FUN = compare_x_to_xminusone, x = directions)
 
 sum(results == "TRUE")
 
